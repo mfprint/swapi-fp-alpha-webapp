1ba3825cf65de61cf840acf9e0814200d5843928
Martín Figueroa Padilla <martnfigueroa21@gmail.com>
2020-05-10 20:30:21 -0500

    Base API url update

--- src/APIs/baseAPI.js
+++ src/APIs/baseAPI.js
@@ -2,6 +2,6 @@ import Axios from 'axios';
 
 export default Axios.create({
     baseURL: process.env.NODE_ENV === 'production'
-        ? 'https://mfprint-swapi.herokuapp.com/api'
+        ? 'http://ec2-18-188-126-183.us-east-2.compute.amazonaws.com:8010/api'
         : 'http://localhost:8001/api'
 });
